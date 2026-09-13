fe4485c78e463c6e24a45a3a063b5a0959a83c36
SuryanRaj <suryanraj056@gmail.com>
2026-09-14 01:13:07 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0,buyprice=prices[0];
+        for(int i=1;i<prices.size();i++)
+        {
+            if(prices[i]<buyprice)
+            {
+                buyprice=prices[i];
+            }
+            else
+            {
+                int currentprofit=prices[i]-buyprice;
+                profit=max(currentprofit,profit);
+            }
+        }
+        return profit;
+    }
+};
